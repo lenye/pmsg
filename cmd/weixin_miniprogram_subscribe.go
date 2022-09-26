2fa0e9cbcf4301da38d19a7b72fbdcc9f78e6337
lenye <pyweb@qq.com>
2022-09-27 00:21:56 +0800

    refactor: 微信小程序订阅消息

--- cmd/weixin_miniprogram_subscribe.go
+++ cmd/weixin_miniprogram_subscribe.go
@@ -1,29 +1,13 @@
 package cmd
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 
-	"github.com/lenye/pmsg/pkg/http/client"
-	"github.com/lenye/pmsg/pkg/weixin"
+	"github.com/lenye/pmsg/pkg/flags"
 	"github.com/lenye/pmsg/pkg/weixin/miniprogram/message"
-	"github.com/lenye/pmsg/pkg/weixin/token"
-)
-
-const (
-	namePage             = "page"
-	nameMiniProgramState = "miniprogram_state"
-	nameLanguage         = "lang"
-)
-
-var (
-	page             string
-	miniProgramState string
-	language         string
 )
 
 // weiXinMiniProgramSubCmd 微信小程序订阅消息
@@ -33,7 +17,19 @@ var weiXinMiniProgramSubCmd = &cobra.Command{
 	Short:   "publish weixin miniprogram subscribe message",
 	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := WeiXinMiniProgramSendSubscribe(args); err != nil {
+		arg := message.CmdMiniSendSubscribeParams{
+			UserAgent:        userAgent,
+			AccessToken:      accessToken,
+			AppID:            appID,
+			AppSecret:        appSecret,
+			ToUser:           openID,
+			TemplateID:       templateID,
+			MiniProgramState: miniProgramState,
+			Page:             page,
+			Language:         language,
+			Data:             args[0],
+		}
+		if err := message.CmdMiniProgramSendSubscribe(&arg); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	},
@@ -44,82 +40,13 @@ func init() {
 
 	weiXinSetAccessTokenFlags(weiXinMiniProgramSubCmd)
 
-	weiXinMiniProgramSubCmd.Flags().StringVarP(&openID, nameOpenID, "o", "", "weixin user open id (required)")
-	weiXinMiniProgramSubCmd.MarkFlagRequired(nameOpenID)
+	weiXinMiniProgramSubCmd.Flags().StringVarP(&openID, flags.NameOpenID, "o", "", "weixin user open id (required)")
+	weiXinMiniProgramSubCmd.MarkFlagRequired(flags.NameOpenID)
 
-	weiXinMiniProgramSubCmd.Flags().StringVarP(&templateID, nameTemplateID, "p", "", "weixin template id (required)")
-	weiXinMiniProgramSubCmd.MarkFlagRequired(nameTemplateID)
+	weiXinMiniProgramSubCmd.Flags().StringVarP(&templateID, flags.NameTemplateID, "p", "", "weixin template id (required)")
+	weiXinMiniProgramSubCmd.MarkFlagRequired(flags.NameTemplateID)
 
-	weiXinMiniProgramSubCmd.Flags().StringVarP(&miniProgramState, nameMiniProgramState, "g", "", "miniprogram_state")
-	weiXinMiniProgramSubCmd.Flags().StringVar(&page, namePage, "", "page")
-	weiXinMiniProgramSubCmd.Flags().StringVar(&language, nameLanguage, "", "language")
-}
-
-// WeiXinMiniProgramSendSubscribe 发送微信小程序订阅消息
-func WeiXinMiniProgramSendSubscribe(args []string) error {
-
-	if accessToken == "" {
-		if appID == "" {
-			return ErrMultiRequiredOne
-		}
-	}
-
-	var dataItem map[string]message.SubscribeDataItem
-	buf := bytes.NewBufferString("")
-	buf.WriteString(args[0])
-	if buf.String() != "" {
-		if err := json.Unmarshal(buf.Bytes(), &dataItem); err != nil {
-			return fmt.Errorf("invalid json format, %v", err)
-		}
-		for k, v := range dataItem {
-			if v.Value == "" {
-				return fmt.Errorf("data %v.value not set", k)
-			}
-		}
-	}
-
-	msg := message.SubscribeMessage{
-		ToUser:           openID,
-		TemplateID:       templateID,
-		Data:             dataItem,
-		MiniProgramState: message.MiniProgramStateFormal,
-		Language:         message.LanguageZhCN,
-	}
-
-	if page != "" {
-		msg.Page = page
-	}
-
-	if language != "" {
-		if err := message.ValidateLanguage(language); err != nil {
-			return fmt.Errorf("invalid flags %s: %v", nameLanguage, err)
-		}
-		msg.Language = language
-	}
-
-	if miniProgramState != "" {
-		if err := message.ValidateMiniProgramState(miniProgramState); err != nil {
-			return fmt.Errorf("invalid flags %s: %v", nameMiniProgramState, err)
-		}
-		msg.MiniProgramState = miniProgramState
-	}
-
-	if userAgent != "" {
-		client.UserAgent = userAgent
-	}
-
-	if accessToken == "" {
-		accessTokenResp, err := token.GetAccessToken(appID, appSecret)
-		if err != nil {
-			return err
-		}
-		accessToken = accessTokenResp.AccessToken
-	}
-
-	if err := message.SendSubscribe(accessToken, &msg); err != nil {
-		return err
-	}
-	fmt.Println(weixin.MessageOK)
-
-	return nil
+	weiXinMiniProgramSubCmd.Flags().StringVarP(&miniProgramState, flags.NameMiniProgramState, "g", "", "miniprogram_state")
+	weiXinMiniProgramSubCmd.Flags().StringVar(&page, flags.NamePage, "", "page")
+	weiXinMiniProgramSubCmd.Flags().StringVar(&language, flags.NameLanguage, "", "language")
 }
---
+++ pkg/weixin/miniprogram/message/subscribe_cmd.go
@@ -0,0 +1,100 @@
+package message
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/lenye/pmsg/pkg/flags"
+	"github.com/lenye/pmsg/pkg/http/client"
+	"github.com/lenye/pmsg/pkg/weixin"
+	"github.com/lenye/pmsg/pkg/weixin/token"
+)
+
+type CmdMiniSendSubscribeParams struct {
+	UserAgent        string
+	AccessToken      string
+	AppID            string
+	AppSecret        string
+	ToUser           string
+	TemplateID       string
+	MiniProgramState string
+	Page             string
+	Language         string
+	Data             string
+}
+
+func (t *CmdMiniSendSubscribeParams) Validate() error {
+	if t.AccessToken == "" && t.AppID == "" {
+		return flags.ErrMultiRequiredOne
+	}
+	if t.Language != "" {
+		if err := ValidateLanguage(t.Language); err != nil {
+			return fmt.Errorf("invalid flags %s: %v", flags.NameLanguage, err)
+		}
+	}
+
+	if t.MiniProgramState != "" {
+		if err := ValidateMiniProgramState(t.MiniProgramState); err != nil {
+			return fmt.Errorf("invalid flags %s: %v", flags.NameMiniProgramState, err)
+		}
+	}
+
+	return nil
+}
+
+// CmdMiniProgramSendSubscribe 发送微信小程序订阅消息
+func CmdMiniProgramSendSubscribe(arg *CmdMiniSendSubscribeParams) error {
+
+	if err := arg.Validate(); err != nil {
+		return err
+	}
+
+	var dataItem map[string]SubscribeDataItem
+	buf := bytes.NewBufferString("")
+	buf.WriteString(arg.Data)
+	if buf.String() != "" {
+		if err := json.Unmarshal(buf.Bytes(), &dataItem); err != nil {
+			return fmt.Errorf("invalid json format, %v", err)
+		}
+		for k, v := range dataItem {
+			if v.Value == "" {
+				return fmt.Errorf("data %v.value not set", k)
+			}
+		}
+	}
+
+	msg := SubscribeMessage{
+		ToUser:           arg.ToUser,
+		TemplateID:       arg.TemplateID,
+		Data:             dataItem,
+		Page:             arg.Page,
+		MiniProgramState: MiniProgramStateFormal,
+		Language:         LanguageZhCN,
+	}
+
+	if arg.Language != "" {
+		msg.Language = arg.Language
+	}
+
+	if arg.MiniProgramState != "" {
+		msg.MiniProgramState = arg.MiniProgramState
+	}
+
+	client.UserAgent = arg.UserAgent
+
+	if arg.AccessToken == "" {
+		accessTokenResp, err := token.GetAccessToken(arg.AppID, arg.AppSecret)
+		if err != nil {
+			return err
+		}
+		arg.AccessToken = accessTokenResp.AccessToken
+	}
+
+	if err := SendSubscribe(arg.AccessToken, &msg); err != nil {
+		return err
+	}
+	fmt.Println(weixin.MessageOK)
+
+	return nil
+}
