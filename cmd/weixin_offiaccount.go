ec4d908af04e050dd367e857795086ddd3dbd426
lenye <pyweb@qq.com>
2022-12-10 11:53:53 +0800

    fix: 微信公众号命令别名mp更改为oa

--- cmd/weixin_offiaccount.go
+++ cmd/weixin_offiaccount.go
@@ -21,7 +21,7 @@ import (
 // weiXinOfficialAccountCmd 微信公众号
 var weiXinOfficialAccountCmd = &cobra.Command{
 	Use:     "offiaccount",
-	Aliases: []string{"mp"},
+	Aliases: []string{"oa"},
 	Short:   "weixin official account",
 }
 
